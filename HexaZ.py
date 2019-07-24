791d6c07015f2f8fb7868b5d661d572d71ae98e0
Lawliet <jerry_howl_lawliet@hotmail.com>
2019-07-24 05:36:15 -0500

    HexaZ v1.0.2
    v1.0.2

--- HexaZ.py
+++ HexaZ.py
@@ -7,28 +7,38 @@ pause = lambda: os.system('Pause > NUL')
 
 def estimateTotal(l_str, lenght):
 	val = 0
-	for x in range(1,lenght+1):
-		val += (l_str ** x)
+	if _type == 'pyramid':
+		for x in range(1,lenght+1):
+			val += (l_str ** x)
+	elif _type == 'exact':
+		val = (l_str ** lenght)
 	return val
 
 def estimateBytesTotal(estimate, l_str, lenght):
-	# val = Cantidad de bytes generados por los saltos de linea.
-	# Son 2 bytes por salto, porque son 2 caracteres: \n
-	val = estimate*2
-	for x in range(1,lenght+1):
-		val += ((l_str ** x)*x)
+	val = 0
+	if _type == 'pyramid':
+		# Cantidad de bytes generados por los saltos de linea.
+		# Son 2 bytes por salto, porque son 2 caracteres: \n
+		val = estimate*2
+		for x in range(1,lenght+1):
+			val += ((l_str ** x) * x)
+	elif _type == 'exact':
+		val = (estimate*2) + (estimate*lenght)
 	return val
 
 def getSizeFile(total_bytes):
-	total = 0
+	total = total_bytes
 	_type = 'Bytes'
-	if total_bytes > (1024**3):
+	if total_bytes >= (1024**4):
+		total = total_bytes / (1024**3)
+		_type = 'Tb'
+	elif total_bytes >= (1024**3):
 		total = total_bytes / (1024**3)
 		_type = 'Gb'
-	elif total_bytes > (1024**2):
+	elif total_bytes >= (1024**2):
 		total = total_bytes / (1024**2)
 		_type = 'Mb'
-	elif total_bytes > 1024:
+	elif total_bytes >=1024:
 		total = total_bytes / 1024
 		_type = 'Kb'
 	
@@ -45,21 +55,19 @@ def normalizeNumber(number):
 		cont += 1
 	return val[::-1]
 	
-
 def getPorcent():
 	porcent = (cont / estimate) * 100
 	return porcent
 
-def piramidData(s, c):
-	
+
+
+def pyramidData(s, c):
 	global cont, sec
 	
 	for l in s:
-		
 		l = c+l
 		cont += 1
 		fil.write(l+'\n')
-		
 		if not len(l) == longy:
 			piramidData(s, l)
 	
@@ -70,13 +78,32 @@ def piramidData(s, c):
 		sys.stdout.write('\r Total: {:.2f}%    '.format(getPorcent()))
 	
 
+def exactData(s, c):
+	global cont, sec
+	
+	for l in s:
+		l = c+l
+		if len(l) == longy:
+			cont += 1
+			fil.write(l+'\n')
+		else:
+			exactData(s, l)
+	
+	sec_n = int(datetime.now().strftime('%S'))
+	
+	if sec_n > sec or (sec_n == 0 and not sec == 0):
+		sec = sec_n
+		sys.stdout.write('\r Total: {:.2f}%    '.format(getPorcent()))
+
+
 
 if __name__ == '__main__':
 	
-	ver = 'v1.0.1'
+	ver = 'v1.0.2'
 	sec = 0
 	string = '0123456789ABCDEF'
-	longy = 8	# Modificar por la cantidad que se desee.
+	_type = 'exact'		# ['pyramid', 'exact']
+	longy = 8			# Modificar por la cantidad que se desee.
 	cont = 0
 	estimate = estimateTotal(len(string), longy)
 	total_bytes = estimateBytesTotal(estimate, len(string), longy)
@@ -84,15 +111,20 @@ if __name__ == '__main__':
 	
 	print('\n\n Generador de Diccionario Hexadecimal. {}'.format(ver))
 	print('\n Total Estimado de Cadenas: {}'.format(normalizeNumber(estimate)))
-	print('\n Peso Exacto de Salida: ''{:.3f} {} ({} bytes)\n\n\n'.format(
+	if tipo == 'Bytes':
+		print('\n Peso Exacto de Salida: ''{} {} ({} bytes)\n\n\n'.format(
+									espacio, tipo, normalizeNumber(total_bytes)))
+	else:
+		print('\n Peso Exacto de Salida: ''{:.3f} {} ({} bytes)\n\n\n'.format(
 									espacio, tipo, normalizeNumber(total_bytes)))
 	
-	fil = open('hex_len-{}.zion'.format(longy),'w')
-	piramidData(string, '')
+	fil = open('hexaz - len_{}-{}.zion'.format(longy, _type.title()),'w')
+	if _type == 'pyramid': pyramidData(string, '')
+	elif _type == 'exact': exactData(string, '')
+	fil.close()
 	sys.stdout.write('\r Porcentaje Total: {:.2f}%    '.format(getPorcent()))
 	print('\n\n Total de Cadenas Generado: {}    '.format(normalizeNumber(cont)))
 
-	fil.close()
 	
 
 
